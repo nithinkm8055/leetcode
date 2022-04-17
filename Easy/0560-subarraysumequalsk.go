fd1d1bf5c00c10866aa416662c4ef51a93051d50
Nithin KM <nithin.kaiprath.meethileveetil@nl.abnamro.com>
2022-04-17 13:03:32 +0200

    subarray equals sum

---
+++ Easy/0560-subarraysumequalsk.go
@@ -0,0 +1,15 @@
+package Easy
+
+func SubarraySum(nums []int, k int) int {
+	counter := 0
+	for i := range nums {
+		currSum := 0
+		for j := i; j < len(nums); j++ {
+			currSum = currSum + nums[j]
+			if currSum == k{
+				counter++
+			}
+		}
+	}
+	return counter
+}
