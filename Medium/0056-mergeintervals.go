7cdbf30bf792c44a28a9b0c7cfdc47f6b3f8fda0
nithinkm8055 <nithinkm.tcs@gmail.com>
2023-11-12 23:17:58 +0100

    Arrays: merge intervals

--- Medium/0056-mergeintervals.go
+++ Medium/0056-mergeintervals.go
@@ -1,17 +1,31 @@
 package Medium
 
-func Merge(intervals [][]int) [][]int {
-	//fmt.Println(sort.Ints(intervals))
+import "sort"
+
+func merge(intervals [][]int) [][]int {
+
+	sort.SliceStable(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	newInterval := intervals[0]
+
 	result := make([][]int, 0)
+
 	for i := range intervals {
-		for j := i + 1; j < len(intervals); j++ {
-			temp := make([]int, 0)
-			if intervals[i][1] <= intervals[j][1] && intervals[i][1] >= intervals[j][0] {
-				temp = append(temp, intervals[i][0])
-				temp = append(temp, intervals[j][1])
-				result = append(result, temp)
-			}
+
+		if newInterval[1] < intervals[i][0] {
+			result = append(result, newInterval)
+			newInterval = intervals[i]
+		} else if newInterval[0] > intervals[i][1] {
+			result = append(result, intervals[i])
+		} else {
+			newInterval[0] = min(newInterval[0], intervals[i][0])
+			newInterval[1] = max(newInterval[1], intervals[i][1])
 		}
 	}
+
+	result = append(result, newInterval)
+
 	return result
 }
